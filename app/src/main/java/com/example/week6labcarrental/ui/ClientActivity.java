73e8c0f1989751f2708dffa7fbc873c8cec28d0f
jpdestrok <51759471+jpdestrok@users.noreply.github.com>
2019-06-26 18:25:37 -0700

    add recyclerview

--- app/src/main/java/com/example/week6labcarrental/ui/ClientActivity.java
+++ app/src/main/java/com/example/week6labcarrental/ui/ClientActivity.java
@@ -1,11 +1,17 @@
 package com.example.week6labcarrental.ui;
 
 import android.app.DatePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,31 +20,57 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.example.week6labcarrental.R;
+import com.example.week6labcarrental.adapter.ItemClickListener;
+import com.example.week6labcarrental.adapter.MyRecyclerAdapter;
 import com.example.week6labcarrental.controller.Authentication;
+import com.example.week6labcarrental.firebase.CarCollection;
+import com.example.week6labcarrental.model.Car;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
-public class ClientActivity extends AppCompatActivity {
+public class ClientActivity extends AppCompatActivity implements ItemClickListener {
 
-    Button btnPickup, btnReturn;
+    // buttons
+    Button btnPickup, btnContinue;
+    // date
     DatePickerDialog.OnDateSetListener setListener;
+
     TextView txtpickup, txtreturn;
     String pickupdate, returndate; // serves as holder for the dates
     // Firebase auth
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    public static final String COLLECTION_NAME = "cars";
+    private final String[] key = {"carId", "category", "pricePerHour", "pricePerDay", "carMake", "carModel", "color", "availability"};
+
+    ArrayList<Car> carsList;
+
+    BroadcastReceiver response;
+    RecyclerView recyclerView;
+    MyRecyclerAdapter carRecyclerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        carsList = new ArrayList<>();
+        initialize();
+        recyclerView =  findViewById(R.id.myRec);
+        carRecyclerAdapter = new MyRecyclerAdapter(carsList, this);
+        recyclerView.setAdapter(carRecyclerAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(ClientActivity.this, 1));
 
+        btnContinue = findViewById(R.id.btnContinue);
         btnPickup = findViewById(R.id.btnPickup);
-        btnReturn = findViewById(R.id.btnReturn);
         txtpickup = findViewById(R.id.txtPickup);
-        txtreturn = findViewById(R.id.txtReturn);
 
         Calendar c = Calendar.getInstance();
         final int year = c.get(Calendar.YEAR);
@@ -71,30 +103,52 @@ public class ClientActivity extends AppCompatActivity {
             }
         };
 
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            Calendar cal = Calendar.getInstance();
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        ClientActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int day) {
-                        month += 1;
-                        String date = day + "/" + month + "/" + year;
-                        returndate = date;
-                        txtreturn.setText(date);
-                    }
-                }, year, month, day);
-                datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis());
-                datePickerDialog.show();
+
             }
         });
+
+
+        response = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (intent.getAction()) {
+                    case CarCollection.LOAD_CAR_DATA_DONE:
+                        //get car Data from intent
+                        carsList = (ArrayList<Car>) intent.getSerializableExtra("cars_data");
+                        Log.i("carlist", String.valueOf(carsList.size()));
+                        carRecyclerAdapter.changeData(carsList);
+                        carRecyclerAdapter.notifyDataSetChanged();
+                        break;
+                }
+            }
+        };
+
     }
     @Override
     protected void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         Authentication.checkSignIn(mAuth, this);
+    }
+
+    @Override
+    protected void onPause() {
+        // Unregister since the activity is paused.
+        super.onPause();
+        unregisterReceiver(response);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // An IntentFilter can match against actions, categories, and data
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(CarCollection.LOAD_CAR_DATA_DONE);
+        registerReceiver(response,filter);
     }
 
     @Override
@@ -115,4 +169,27 @@ public class ClientActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void initialize() {
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder().build();
+        db.setFirestoreSettings(settings);
+        //get all the cars
+        CarCollection.getAllCars(this,db);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Car clickedCar = carsList.get(position);
+        Intent i = new Intent(this,sale2.class);
+        i.putExtra("car",clickedCar);
+        startActivity(i);
+    }
+
+    @Override
+    public void onItemLongClick(View view, int position) {
+
+    }
+
+
 }
